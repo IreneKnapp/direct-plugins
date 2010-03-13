8e82c09d5bd0558fd663c3d860e39624241f330b
Irene Knapp <irene.knapp@icloud.com>
2010-03-13 01:12:26 +0000

    stuff

--- System/Plugins.hs
+++ System/Plugins.hs
@@ -6,6 +6,7 @@ import qualified DynFlags
 import qualified Encoding
 import qualified FastString
 import qualified GHC
+import qualified GHC.Exts
 import qualified GHC.Paths (libdir)
 import qualified Linker
 import qualified Module
@@ -20,17 +21,11 @@ main = GHC.defaultErrorHandler DynFlags.defaultDynFlags $ do
            flags <- GHC.getSessionDynFlags
            GHC.setSessionDynFlags flags
            
-           (flags, explicitPackages) <- liftIO $ Packages.initPackages flags
-           liftIO $ putStrLn $ show $ map Module.packageIdString explicitPackages
+           (flags, _) <- liftIO $ Packages.initPackages flags
            
            liftIO $ Linker.initDynLinker flags
-           liftIO $ Linker.showLinkerState
-           let packageId = Module.fsToPackageId (FastString.mkFastString "mtl-1.1.0.2")
+           let packageId = Module.fsToPackageId (FastString.mkFastString "hello-1.0")
            liftIO $ Linker.linkPackages flags [packageId]
-           let packageId = Module.fsToPackageId (FastString.mkFastString "FruitTartInterface-1.0")
-           liftIO $ Linker.linkPackages flags [packageId]
-           
-           liftIO $ Linker.showLinkerState
            
            liftIO $ testSymbol $
                            "__stginit_"
@@ -47,6 +42,21 @@ main = GHC.defaultErrorHandler DynFlags.defaultDynFlags $ do
            liftIO $ testSymbol $ "__stginit_base_DataziBits_"
            liftIO $ testSymbol $ "__stginit_mtlzm1zi1zi0zi2_ControlziMonadziCont"
            liftIO $ testSymbol $ "FruitTartInterfacezm1zi0_NetworkziFruitTartziBase_canonicalURL_closure"
+           liftIO $ testSymbol $ "FruitTartInterfacezm1zi0_NetworkziFruitTartziBase_canonicalURL_closure"
+
+           maybePointer <- ObjLink.lookupSymbol $
+                           (Encoding.zEncodeString "hello-1.0")
+                           ++ "_"
+                           ++ (Encoding.zEncodeString "Hello")
+                           ++ "_"
+                           ++ (Encoding.zEncodeString "hello")
+                           ++ "_closure"
+           case maybePointer of
+             Nothing -> putStrLn $ "Unable to load hello."
+             Just pointer -> do
+               case GHC.Exts.addrToHValue# pointer of
+                 (# hValue #) -> putStrLn $ hValue
+           putStrLn "foom"
 
 testSymbol :: String -> IO ()
 testSymbol symbol = do
